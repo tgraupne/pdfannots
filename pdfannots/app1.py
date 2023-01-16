0e806837f8136a1222c83314eadbbcab8a29b38a
Tom Graupner <mail@tomgraupner.de>
2023-01-16 09:40:51 -0500

    WIP tkinter GUI

---
+++ pdfannots/app1.py
@@ -0,0 +1,72 @@
+import os
+import tkinter
+from tkinter import Tk, Scrollbar, Text, RIGHT, Button, filedialog, messagebox, END, Frame, Label
+from tkinter.ttk import Style
+
+
+class App:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("PDF Notes Exporter")
+
+        self.open_file_button = Button(self.root, text='Open PDF File', command=self.event_open_file)
+        self.open_file_button.grid(row=0, column=0, rowspan=1, columnspan=1)
+
+        self.open_preview_button = Button(self.root, text='Open Preview', command=self.event_open_preview)
+        self.open_preview_button.grid(row=0, column=1, rowspan=1, columnspan=1)
+
+        self.export_annotations_button = Button(self.root, text='Export', command=self.event_export)
+        self.export_annotations_button.grid(row=0, column=2, rowspan=1, columnspan=1)
+
+        self.file_name_label = Label(self.root, text='...', borderwidth=1, relief="ridge")
+
+        # Init Frame for Annotation Preview
+        self.annotation_preview_frame = Frame(self.root)
+        self.annotation_preview_frame.grid(row=3, column=0)
+        self.annotation_preview_scrollbar = Scrollbar(self.annotation_preview_frame, orient='vertical')
+        self.annotation_preview = Text(self.annotation_preview_frame, yscrollcommand=self.annotation_preview_scrollbar.set)
+
+
+    def event_open_file(self):
+        filename = filedialog.askopenfilename(initialdir="/",
+                                              title="Select a File",
+                                              filetypes=(("PDF files",
+                                                          "*.pdf"),
+                                                         ("all files",
+                                                          "*.*")))
+
+        # Change label contents
+        self.file_name_label.configure(text=filename)
+        self.file_name_label.pack()
+
+    def event_open_preview(self):
+        annotations = self.get_annotations()
+
+
+
+    def show_annotation_preview_frame(self):
+        # self.annotation_preview_frame.pack()
+        # self.annotation_preview_scrollbar.config(command=self.annotation_preview.yview)
+        self.annotation_preview_scrollbar.pack()
+        self.annotation_preview.pack()
+
+    def show(self):
+        self.root.mainloop()
+
+    def event_export(self):
+        folder_selected = filedialog.askdirectory()
+        if len(folder_selected) > 0:
+            output_file = folder_selected + '/export.md'
+            if len(self.textArea.get("1.0", "end-1c")) > 0:
+                with open(output_file, 'w') as f:
+                    f.write(self.textArea.get("1.0", "end-1c"))
+                messagebox.showinfo("pdfannots", "Sucess")
+            else:
+                messagebox.showwarning("pdfannots", "Without Output. Please, upload a pdf file")
+        # else:
+        #     messagebox.showwarning("pdfannots", "Choose a folder")
+
+
+if __name__ == '__main__':
+    app = App()
+    app.show()
